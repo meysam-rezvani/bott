447b7b9387aebb7183cd3818fe43f80349fbac75
meytham <97456232+meysam-rezvani@users.noreply.github.com>
2025-02-18 12:10:13 -0800

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,27 @@
+const TelegramBot = require('node-telegram-bot-api');
+const moment = require('moment-jalaali');
+
+
+const token = "7930873100:AAE1USB6Sg-eHsMbsWZo7fQMraeF9MzIhC0";
+const chatId = "dfgbr";
+
+const bot = new TelegramBot(token, { polling: true });
+
+function getTodayDate() {
+    return moment().format('jYYYY/jMM/jDD');
+}
+
+function sendDailyCalendar(chatId) {
+    const today = getTodayDate();
+    const message = `📅 امروز: ${today} \n یک روز خوب داشته باشید!`;
+    bot.sendMessage(chatId, message);
+}
+
+// ارسال پیام هر روز در ساعت 8 صبح به وقت محلی سرور
+const schedule = require('node-schedule');
+schedule.scheduleJob('0 8 * * *', () => sendDailyCalendar(chatId));
+
+// پاسخ به پیام‌های ارسال شده به ربات
+bot.on('message', (msg) => {
+    sendDailyCalendar(msg.chat.id);
+});
